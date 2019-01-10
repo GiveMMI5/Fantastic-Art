3b05da5e67ddf208010c6a6b63f4af2ae5d38084
Unknown <manon.bin@orange.fr>
2019-01-10 15:50:12 +0100

    theme vide

---
+++ inc/custom-functions.php
@@ -0,0 +1,23 @@
+<?php
+
+// =============== Ne pas modifier directement ce fichier ! ===================
+
+/**
+ * Fonctions codées par Sarah
+ */
+require get_template_directory() . '/inc/custom-functions-sarah.php';
+
+/**
+ * Fonctions codées par Manon
+ */
+require get_template_directory() . '/inc/custom-functions-manon.php';
+
+/**
+ * Fonctions codées par Corentin
+ */
+require get_template_directory() . '/inc/custom-functions-corentin.php';
+
+/**
+ * Fonctions codées par Thomas
+ */
+require get_template_directory() . '/inc/custom-functions-thomas.php';
