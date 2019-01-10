79304ee01c96c4b890cff94742b105d331e9bcb8
Corentin <corentin19512@gmail.com>
2019-01-10 16:26:52 +0100

    Theme vide

---
+++ inc/custom-functions-sarah.php
@@ -0,0 +1,7 @@
+<?php
+/* -- exemple appel Javascript (remplacer ce qui est entre < >)
+   -- "array ( 'jquery' )" permet d'utiliser JQuery, qwui est déjà intégré à Wordpress (il y a d'autres frameworks)
+wp_enqueue_script( '<Nom_du_script>', get_template_directory_uri() . '/js/<Nom_du_fichier_.js>', array ( 'jquery' ), 1.0, true); */
+
+/* -- exemple appel CSS (remplacer ce qui est entre < >)
+wp_enqueue_style( '<Nom_du_script>', get_template_directory_uri() . '/layouts/<Nom_du_fichier_.css>',false,'1.1','all'); */
